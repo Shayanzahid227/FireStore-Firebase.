aba288c20ae3a1a968ccef722d6278fc8c207083
Shayan Zahid <sshayanzahid227@gmail.com>
2026-05-12 10:20:01 -0700

    updated code

--- lib/ui/auth/login/login_screen.dart
+++ lib/ui/auth/login/login_screen.dart
@@ -35,6 +35,7 @@ class _LogInScreenState extends State<LogInScreen> {
                 ),
                 child: Form(
                   key: model.formKey,
+                  //
                   child: Column(
                     children: [
                       Align(
